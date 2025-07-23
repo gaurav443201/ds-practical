d946f6443ccaac5c12dd6f7f277b79bffe0abffa
CRAZYSHIT <NAVGHAREGAURAV80@GMAIL.COM>
2025-07-23 10:26:49 +0530

    Update ecom.py

--- ecom.py
+++ ecom.py
@@ -1,72 +1,59 @@
-def linear(ecom,a):
-	key = int(input("ENTER THE ELEMENT WHICH IS TO BE SEARCH :"))
-	for i in range(a):
-		if key in ecom :
-			print("ELEMENT FOUND")
-			break
-		else:
-			print("ELEMENT NOT FOUND")		
-			break
+def linear(ecom):
+    key = int(input("ENTER THE ELEMENT WHICH IS TO BE SEARCHED: "))
+    if key in ecom:
+        print("ELEMENT FOUND")
+    else:
+        print("ELEMENT NOT FOUND")
+
+
 def binary(ecom):
-	key = int(input("ENTER THE ELEMENT WHICH IS TO BE SEARCHED: "))
+    key = int(input("ENTER THE ELEMENT WHICH IS TO BE SEARCHED: "))
     
-	low = 0
-	high = len(ecom) - 1
+    low = 0
+    high = len(ecom) - 1
     
-	while low <= high:
-		mid = (low + high) // 2
-		print(f"CURRENT MID INDEX IS: {mid}")
+    while low <= high:
+        mid = (low + high) // 2
+        print(f"CURRENT MID INDEX IS: {mid}")
 
-		if ecom[mid] == key:
-			print("ELEMENT FOUND AT INDEX", mid)
-			return mid
+        if ecom[mid] == key:
+            print("ELEMENT FOUND AT INDEX", mid)
+            return
+        elif ecom[mid] < key:
+            low = mid + 1
+        else:
+            high = mid - 1
 
-		elif ecom[mid] < key:
-			print("SEARCHING AT RIGHT SIDE ")
-			low = mid + 1
-
-		else:
-			print("SEARCHING AT LEFT SIDE")
-			high = mid - 1
-
-		print("ELEMENT NOT FOUND")
-		return -1
+    print("ELEMENT NOT FOUND")
 
 
+# Main program
+ecom = []
+n = int(input("ENTER TOTAL NUMBER OF CUSTOMERS: "))
 
-
-
-
-#main program
-ecom=[]
-n=int(input("ENTER A TOTAL NUMBER OF COSTUMERS:"))
-
-print("\n")
 for i in range(n):
-	
-	a=int(input("ENTER ACCOUNT ID FOR COSTUMER:"))
+    a = int(input("ENTER ACCOUNT ID FOR CUSTOMER: "))
+    ecom.append(a)
 
-	ecom.append(a)
-	
-	
-	
-print(ecom)
+ecom.sort()
+print("SORTED ACCOUNT IDS:", ecom)
 
 while True:
+    print("""\n************** E-COMMERCE SYSTEM ****************
+    1. LINEAR SEARCH
+    2. BINARY SEARCH
+    3. EXIT""")
+    
+    ch = int(input("ENTER YOUR CHOICE: "))
+    
+    if ch == 1:
+        linear(ecom)
+    elif ch == 2:
+        binary(ecom)
+    elif ch == 3:
+        print("THANK YOU!")
+        break
+    else:
+        print("INVALID CHOICE!")
 
-	print("""**************E-COMMERSE SYSTEM ****************")
-	1. LINEAR SEARCH
-	2. BINARY SEARCH
-	3. EXIT""")
-	
-	ch= int(input("ENTER YOUR CHOICE:"))
-	if (ch==1):
-		linear(ecom)
-	elif (ch==2):
-		binary(ecom)
-	elif (ch==3):
-		print("THANK YOU !")
-		break
-	else:
-		print("INVALID CHOICE  !")
 		
