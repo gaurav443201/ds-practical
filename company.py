87bb810b58f435705a745b1e3645244a5c5c8ba2
CRAZYSHIT <NAVGHAREGAURAV80@GMAIL.COM>
2025-07-28 14:37:36 +0530

    Create company.py

---
+++ company.py
@@ -0,0 +1,64 @@
+def selection(com):
+	for i in range(len(com)):
+		min_index = i
+		for j in range(i+1, len(com)):
+			if com[j] < com[min_index]:
+				min_index = j
+		com[i], com[min_index] = com[min_index], com[i]
+    
+	print("\nSORTED SALARIES (SELECTION SORT):", com)
+    
+	print("TOP 5 HIGHEST SALARIES:")
+	top_five = com[-5:]
+	top_five.reverse()
+	for salary in top_five:
+		print(salary)
+	
+def bubble(com):
+	n = len(com)
+	for i in range(n):
+		for j in range(0, n - i - 1):
+			if com[j] > com[j + 1]:
+				com[j], com[j + 1] = com[j + 1], com[j]
+    
+	print("\nSORTED SALARIES (BUBBLE SORT):", com)
+    
+	print("TOP 5 HIGHEST SALARIES:")
+	top_five =com[-5:]
+	top_five.reverse()
+	for salary in top_five:
+		print(salary)
+
+
+
+#main program
+com = []
+n = int(input("ENTER TOTAL NUMBER OF EMPLOYEE: "))
+
+print("\n")
+for i in range(n):
+		a = float(input(f"ENTER SALLARY FOR EMPLOYEE {i+1}:"))
+		com.append(a)
+
+print(com)
+
+
+while True:
+	print("""\n************** E-COMMERCE SYSTEM ****************
+	1. SELECTION SORTING
+	2. BUBBLE SORTING
+	3. EXIT""")
+    
+	ch = int(input("ENTER YOUR CHOICE: "))
+    
+	if ch == 1:
+		selection(com)
+	elif ch == 2:
+		bubble(com)	
+	elif ch == 3:
+		print("THANK YOU!")
+		break
+	else:
+		print("INVALID CHOICE!")
+
+
